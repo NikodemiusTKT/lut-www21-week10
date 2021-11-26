a3a42cc887fafed280a0337269a4184183761d6f
Teemu <teemuxtanninen@gmail.com>
2021-11-26 04:19:35 +0200

    Test 2: UseEffect should be passed

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function About() {
-  return <div>This is a about component!</div>;
-}
+  let [listItems, setListItems] = useState([]);
 
+  useEffect(() => {
+    let mounted = true;
+    const fetchPosts = async () => {
+      const response = await fetch('http://jsonplaceholder.typicode.com/posts');
+      const posts = await response.json();
+      if (mounted) {
+        setListItems(posts);
+      }
+    };
+    fetchPosts();
+    return () => {
+      mounted = false;
+    };
+  }, []);
+  const postsList = (
+    <ul>
+      {listItems.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+
+  if (listItems) {
+    return <div>{listItems && postsList}</div>;
+  } else {
+    return <div>No posts fetched</div>;
+  }
+}
 export default About;
