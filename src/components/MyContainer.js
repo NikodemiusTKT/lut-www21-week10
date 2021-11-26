25053316c35bcec168a6933de5a898ac39aee260
Teemu <teemuxtanninen@gmail.com>
2021-11-26 07:55:05 +0200

    Task 5 - Higher order component implemented

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,17 +1,7 @@
 import React, { useState } from 'react';
-import MyHOC from './MyHOC';
 
 import { useTranslation, withTranslation, Trans } from 'react-i18next';
-const Component = ({ name }) => {
-  return <div>Hello {name}!</div>;
-};
-const wrappedWithName = MyHOC(Component, { name: 'Kalle' });
 export default function MyContainer() {
   const { t, i18n } = useTranslation();
-  return (
-    <div className="container">
-      {t('index')}
-      {wrappedWithName}
-    </div>
-  );
+  return <div className="container">{t('index')}</div>;
 }
